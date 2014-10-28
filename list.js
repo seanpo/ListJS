ed0a858c3561d78fd0ff65f23fb7fcd8b1b2b814
Sean Po <s.po@live.com>
2014-10-28 04:36:24 -0400

    Added maximum number of elements

--- list.js
+++ list.js
@@ -3,27 +3,34 @@ var ListJS = function($element, $repeater, $container, options) {
     options = {
         starting_count: options.starting_count || 2,
         delete_class: options.delete_class || '.delete',
-        value_class: options.value_class || '.value'
+        value_class: options.value_class || '.value',
+        maximum: options.maximum || 0
     }
 
     return {
         html: $element.html(),
+        count: 0,
 
         addElement: function() {
             $container.append(this.html);
+            this.count++;
         },
 
         getResults: function() {
-            return $(options.value_class).map(function(index, $element) {
+            var array = $(options.value_class).map(function(index, $element) {
                 var value = $($element).val();
                 if (value) return value;
             });
+            array.join = [].join;
+            return array;
         },
 
         start: function() {
             var _this = this;
             $repeater.click(function() {
-                _this.addElement();
+                if (!options.maximum || _this.count < options.maximum) {
+                    _this.addElement();
+                }
             });
             $element.remove();
             for (var i = 0; i < options.starting_count; i++) $repeater.click();
